8ec02aceee9aff8da050326134c6de895c4b5ff0
elgmouri abderrahim <ea.officialbox@gmail.com>
2025-12-28 10:33:39 +0000

    add borrows to admin pages

--- app/Controllers/BorrowsController.php
+++ app/Controllers/BorrowsController.php
@@ -1,12 +1,35 @@
 <?php
 require_once __DIR__ . "/../helpers/Auth.php";
-Auth::userOnly();
+require_once __DIR__ . "/../core/Database.php";
 
+Auth::adminOnly();
 
-class BorrowsController {
-    public function borrows(){
-        $pageTitle = 'Borrows list';
-        $view = '/admin/borrows.php';
-        require_once __DIR__ . '/../Views/layouts/main.php';
+class BorrowsController
+{
+    public function borrows()
+    {
+        $pageTitle = "Borrows";
+        $view = 'admin/borrows.php';
+        $pdo = Database::getConnection();
+
+        $stmt = $pdo->prepare("
+            SELECT 
+                borrows.id,
+                borrows.borrowDate,
+                borrows.returnDate,
+                books.title AS book_title,
+                users.firstName,
+                users.lastName,
+                users.email
+            FROM borrows
+            JOIN books ON borrows.bookId = books.id
+            JOIN users ON borrows.readerId = users.id
+            ORDER BY borrows.borrowDate DESC
+        ");
+
+        $stmt->execute();
+        $borrows = $stmt->fetchAll();
+
+        require __DIR__ . '/../Views/layouts/main.php';
     }
 }
