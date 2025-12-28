2e8a98deb19ac9f6b44afa384875901f201b589e
elgmouri abderrahim <ea.officialbox@gmail.com>
2025-12-28 13:44:40 +0000

    add profile page for readers

--- app/Controllers/ProfileController.php
+++ app/Controllers/ProfileController.php
@@ -10,7 +10,10 @@ require_once __DIR__ . '/../core/Database.php';
 class ProfileController {
     public function profile(){
         $pageTitle = 'Profile';
-        $view = 'admin/profile.php';
+        if($_SESSION['role'] == 'admin')
+            $view = 'admin/profile.php';
+        else
+            $view = 'reader/profile.php';
 
         $pdo = Database::getConnection();
         $stmt = $pdo->prepare("SELECT * FROM users WHERE id = ?");
